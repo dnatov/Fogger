5ce43695e1d3eaec1c093a5c0792e5bfd7483356
dnatov <dnatov@gsdx.us>
2020-02-09 18:17:43 -0800

    Small refactoing of http client commands into helper class

--- FogBugzApi/MainApi.cs
+++ FogBugzApi/MainApi.cs
@@ -10,78 +10,16 @@ namespace FogBugzApi
 {
     public class FogBugzApiWrapper
     {
-        private static readonly HttpClient _client = new HttpClient();
         private int _version = -1;
         private int _minVersion = -1;
-        private string _url;
-        private string _mainUri;
-        private string _token;
+        private string _urlSuffix;
         private Filter _previousFilter;
+
+        internal HttpClientHelper HttpClientHelper;
 
         public int Version { get => _version; set => _version = value; }
         public int MinVersion { get => _minVersion; set => _minVersion = value; }
-        public string Url { get => _mainUri + '/' + _url; set => _url = value; }
-        public string Token { get => _token; }
-
-        private string sendPost(string uri, FormUrlEncodedContent content)
-        {
-            var post = _client.PostAsync(uri, content);
-            post.Wait();
-            var htmlString = post.Result.Content.ReadAsStringAsync();
-            htmlString.Wait();
-            return (htmlString.Result ?? "");
-        }
-
-        /// <summary>
-        /// Performs initial Post on given fogbugz uri. Returns html response string, "" if null"
-        /// </summary>
-        /// <param name="uri"></param>
-        /// <returns></returns>
-        private string postWithoutContent(string uri)
-        {
-            //Use empty content
-            var content = new FormUrlEncodedContent(
-                new Dictionary<string, string>
-                {
-                    {" "," "}
-                });
-            return sendPost(uri, content);
-        }
-
-        private void checkResponseForErrorCode(XElement xml)
-        {
-            //Check for error tag and its attribute
-            var errorElement = xml.Elements().Where(x => x.Name.LocalName == "error").FirstOrDefault();
-            if (errorElement?.HasAttributes ?? false)
-            {
-                errorElement = (xml.Elements().Where(x => x.Name.LocalName == "error").FirstOrDefault());
-                int result;
-                if (Int32.TryParse(errorElement.Attribute("code").Value, out result))
-                {
-                    var errorString = errorElement.Value;
-                    throw new FogbugzException(result, errorString);
-                }
-            }
-        }
-
-        /// <summary>
-        /// Uses existing Url and Token to POST with the command in cmd= and returns an xml response. Raises appropriate exception if the response contains an error tag.
-        /// </summary>
-        /// <param name="command"></param>
-        /// <returns></returns>
-        private XElement postAndGetXml(string command)
-        {
-            var post = postWithoutContent($"{Url}cmd={command}&token={_token}");
-            var xml = XElement.Parse(post);
-            checkResponseForErrorCode(xml);
-            return xml;
-        }
-
-        public FogBugzApiWrapper(string Uri, string Username, string Password)
-        {
-            initializeApi(Uri);
-            logon(Username,Password);
-        }
+        public string Token { get => HttpClientHelper.Token; }
 
         /// <summary>
         /// Grabs the initial Url for all further calls to the API. Gets version info of the FogBugz install as well.
@@ -89,21 +27,18 @@ namespace FogBugzApi
         /// <param name="uri"></param>
         private void initializeApi(string uri)
         {
-            //Store main uri
-            _mainUri = uri;
+            HttpClientHelper = new HttpClientHelper();
 
             //Initial Post
-            var post = postWithoutContent(uri + @"/api.xml");
+            var post = HttpClientHelper.postWithoutContent(uri + @"/api.xml");
             //Store in XElement Obj
             var xml = XElement.Parse(post);
-            checkResponseForErrorCode(xml);
+            HttpClientHelper.checkResponseForErrorCode(xml);
 
             //Parse HTML
             _version = Int32.Parse(xml.Elements().Where(x => x.Name.LocalName == "version").FirstOrDefault().Value);
             _minVersion = Int32.Parse(xml.Elements().Where(x => x.Name.LocalName == "minversion").FirstOrDefault().Value);
-            _url = xml.Elements().Where(x => x.Name.LocalName == "url").FirstOrDefault().Value;
-
-
+            _urlSuffix = xml.Elements().Where(x => x.Name.LocalName == "url").FirstOrDefault().Value;
         }
 
         /// <summary>
@@ -111,17 +46,24 @@ namespace FogBugzApi
         /// </summary>
         /// <param name="user"></param>
         /// <param name="pass"></param>
-        private void logon(string user,string pass)
+        private string logon(string uri, string user,string pass)
         {
             //POST using other private method since this assume token is undefined
-            var post = postWithoutContent($"{Url}cmd=logon&email={user}&password={pass}");
+            var post = HttpClientHelper.postWithoutContent($"{uri}cmd = logon&email={user}&password={pass}");
 
             //Store in XElement Obj
             var xml = XElement.Parse(post);
-            checkResponseForErrorCode(xml);
+            HttpClientHelper.checkResponseForErrorCode(xml);
 
             //Parse HTML for token. FogBugz recommends using this token as long as you can in favor of a new login request.
-            _token = (xml.Elements().Where(x => x.Name.LocalName == "token").FirstOrDefault().Value);
+            return (xml.Elements().Where(x => x.Name.LocalName == "token").FirstOrDefault().Value);
+        }
+
+        public FogBugzApiWrapper(string Uri, string Username, string Password)
+        {
+            initializeApi(Uri);
+            var token = logon(Uri,Username,Password);
+            HttpClientHelper.initHttp(Uri + '/' + _urlSuffix, token);
         }
 
         /// <summary>
@@ -131,24 +73,23 @@ namespace FogBugzApi
         public bool ValidateLogon()
         {
             //Store in XElement Obj
-            var xml = postAndGetXml("logon");
+            var xml = HttpClientHelper.postAndGetXml("logon");
 
             //Parse HTML for token. FogBugz recommends using this token as long as you can in favor of a new login request.
             var token = (xml.Elements().Where(x => x.Name.LocalName == "token").FirstOrDefault().Value);
 
             //Logon is only valid if the token that is returned is the same as the token stored
-            return token.Equals(_token);
+            return token.Equals(HttpClientHelper.Token);
         }
 
         public bool Logoff()
         {
-            var xml = postAndGetXml("logoff");
-            _token = null;
-            _url = null;
-            _mainUri = null;
+            HttpClientHelper.postAndGetXml("logoff");
+            _urlSuffix = null;
             _version = -1;
             _minVersion = -1;
             _previousFilter = null;
+            HttpClientHelper = null;
 
             return true;
         }
@@ -160,7 +101,7 @@ namespace FogBugzApi
         public List<Filter> GetFilters()
         {
             //get the xml response listing all filters
-            var xmlResponse = postAndGetXml("listFilters");
+            var xmlResponse = HttpClientHelper.postAndGetXml("listFilters");
 
             //Grab filters element
             var xmlFilters = xmlResponse.Element(XName.Get("filters", xmlResponse.GetDefaultNamespace().ToString()));
@@ -194,9 +135,11 @@ namespace FogBugzApi
             _previousFilter?.SetNotCurrent();
 
             //Set current filter, reponse should be empty
-            postAndGetXml($"setCurrentFilter&sFilter={newFilter.sFilter}");
+            HttpClientHelper.postAndGetXml($"setCurrentFilter&sFilter={newFilter.sFilter}");
 
             newFilter.SetCurrent();
+
+            _previousFilter = newFilter;
         }
 
         public List<Case> SearchCurrentFilter()
@@ -247,7 +190,7 @@ namespace FogBugzApi
                 sbColumns.Append(col + ",");
             }
             sbColumns.Remove(sbColumns.Length - 1, 1);
-            var xml = postAndGetXml($"search&q={query}&cols={sbColumns.ToString()}&max={max}");
+            var xml = HttpClientHelper.postAndGetXml($"search&q={query}&cols={sbColumns.ToString()}&max={max}");
             Console.WriteLine(xml.ToString());
             foreach(var caseXml in xml.Elements().Elements().Where(c=>c.Name.LocalName == "case"))
             {
